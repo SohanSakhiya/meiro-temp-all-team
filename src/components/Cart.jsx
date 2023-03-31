bdb58abb407bedb1d57850ff8fa733dd010d15b5
Sohan Sakhiya <84366713+SohanSakhiya@users.noreply.github.com>
2023-03-31 10:33:00 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
-
+// hello i an sohan
 import { useStateContext } from '../contexts/ContextProvider';
 import { cartData } from '../data/dummy';
 import { Button } from '.';
